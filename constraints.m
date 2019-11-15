697ef98b39ba3e56c15d511493fc127d52f753a1
Giuseppe <giuseppeonorato96@gmail.com>
2019-11-15 10:26:47 +0100

    cons

--- constraints.m
+++ constraints.m
@@ -12,6 +12,50 @@ f_tank = 0.93; %Usable space
 S_0 = 93.5;
 MTOW_0 = (29323+5000)*9.81;
 
+% Wing planform geometry
+global couplings;
+X0 = couplings.X0;
+W_wing_i = couplings.W_wing_c;
+W_fuel_i = couplings.W_fuel_c;
+x = x.*X0;
+sweep = x(1);
+b = x(2);
+lambda_in = x(3);
+lambda_out = x(4);
+root = x(5);
+epsilon_in = x(6);
+epsilon_out = x(7);
+    
+chord_kink = root*lambda_in;
+tip = chord_kink*lambda_out;
+Y_kink = 5.616;
+sweep_kink = 5.37;
+
+sweep_out = sweep;
+X_kink = root-chord_kink+tand(sweep_kink)*Y_kink;
+sweep_in  = atand(X_kink/Y_kink);
+
+AC.Wing.Geom = [];
+AC.Wing.Geom(1,1) = 0;
+AC.Wing.Geom(1,2) = 0;
+AC.Wing.Geom(1,3) = 0;
+AC.Wing.Geom(1,4) = root;
+AC.Wing.Geom(1,5) = 0;
+AC.Wing.Geom(2,1) = X_kink;
+AC.Wing.Geom(2,2) = Y_kink;
+AC.Wing.Geom(2,3) = 0;
+AC.Wing.Geom(2,4) = root+Y_kink*tand(sweep_kink)-(Y_kink*tand(sweep));
+AC.Wing.Geom(2,4) = chord_kink;
+AC.Wing.Geom(2,5) = epsilon_in;
+AC.Wing.Geom(3,1) = Y_kink*tand(sweep_in)+(b/2-Y_kink)*tand(sweep_out);
+AC.Wing.Geom(3,2) = b/2;
+AC.Wing.Geom(3,3) = 0;
+AC.Wing.Geom(3,4) = tip;
+AC.Wing.Geom(3,5) = epsilon_out;
+
+S_in = (AC.Wing.Geom(1,4)+AC.Wing.Geom(2,4))*AC.Wing.Geom(2,2)*0.5;                      %inboard surface
+S_out = (AC.Wing.Geom(3,4)+AC.Wing.Geom(2,4))*(AC.Wing.Geom(3,2)-AC.Wing.Geom(2,2))*0.5;  %inboard surface
+S = 2*(S_in+S_out);                     %total surfaceMAC
 
 global couplings;
 
